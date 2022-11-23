d77d732872ecc30ade7d61dae83595249a7c85a4
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-11-24 03:27:36 +0800

    last resort changes on deployment path

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let customerJSON = "";
 if (process.env.NODE_ENV !== "production") {
     customerJSON = path.join(__dirname, `customers.json`);
 } else {
-    customerJSON = '/var/task/customers.json';
+    customerJSON = './tmp/customers.json';
 }
 
 // Initiate Express
